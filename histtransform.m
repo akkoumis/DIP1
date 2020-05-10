86729d9b2609105b75d247d7bacc57c2c0ddb2f7
akkoumis <akkoumis@ece.auth.gr>
2020-05-10 20:57:04 +0300

    Fixed histtransform based on the question + Fix bug in pointtransform
    Check the condition at the end of the loop (hist)
    Execute 1b with proper values(point)

--- histtransform.m
+++ histtransform.m
@@ -10,23 +10,27 @@ new_index = 1; % Index for the new values (h, v)
 
 count = 0; % Count to calculate the new h(i)
 
+% Iterate through old values of brightness
 for old_index = 1:size(old_values,1)
     temp = old_values(old_index);
     
+    % Iterate through all the pixels
     for i = 1:size(Y,1)
         for j = 1:size(Y,2)
             if(X(i,j) == temp)
-                new_h = count/total_num_of_pixels;
-                while(new_h >= h(new_index))
-                    new_index = new_index+1;
-                    count = 0;
-                    new_h = count/total_num_of_pixels;
-                end
                 Y(i,j) = v(new_index);
                 count = count + 1;
             end
         end
-    end  
+    end
+    
+    new_h = count/total_num_of_pixels;
+    while(new_h >= h(new_index))
+        new_index = new_index+1;
+        count = 0;
+        new_h = count/total_num_of_pixels;
+    end
+    
 end
 
 end
